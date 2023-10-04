0c485d32fbfb7dd0d144c79a65dc5cdb14126d6b
Meriem Gueffal <mg21aaq@herts.ac.uk>
2023-10-04 19:58:44 +0100

    Add last components

---
+++ pages/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <header class="usa-header usa-header--basic">
+        <div class="usa-nav-container">
+          <div class="usa-navbar">
+            <div class="usa-logo">
+              <em class="usa-logo__text"
+              ><a href="/" title="<Project title>">Earth Surface Mineral Dust Source
+                Investigation ("EMIT")</a></em>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+
+}
