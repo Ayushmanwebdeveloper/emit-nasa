7180d1de6be81743c40d88ba47a0608edd128412
Meriem Gueffal <mg21aaq@herts.ac.uk>
2023-10-05 01:13:07 +0100

    Fix footer socials

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -9,18 +9,16 @@ export default function Footer(){
     <div class="grid-container usa-footer__return-to-top">
       <a href="#">Return to top</a>
     </div>
+
     <div class="usa-footer__primary-section">
       <nav class="usa-footer__nav" aria-label="Footer navigation">
         <ul class="grid-row grid-gap">
-          <li
-            class="
-          mobile-lg:grid-col-4
-          desktop:grid-col-auto
-          usa-footer__primary-content
-        ">
+
+          <li class="mobile-lg:grid-col-4 desktop:grid-col-auto usa-footer__primary-content">
             <a class="usa-footer__primary-link" href="javascript:void(0);"
             >&lt;Primary link&gt;</a>
           </li>
+
           <li
             class="
           mobile-lg:grid-col-4
@@ -54,6 +52,8 @@ export default function Footer(){
         </ul>
       </nav>
     </div>
+
+
     <div class="usa-footer__secondary-section">
       <div class="grid-container">
         <div class="grid-row grid-gap">
@@ -66,63 +66,58 @@ export default function Footer(){
           >
             <div class="mobile-lg:grid-col-auto">
               <img
-                class="usa-footer__logo-img"
-                src="/assets/img/logo-img.png"
-                alt=""
-              />
+                class="usa-footer__logo-img" src="assets\img\logo-img.png" alt="" />
             </div>
+
+
             <div class="mobile-lg:grid-col-auto">
               <p class="usa-footer__logo-heading">&lt;Name of Agency&gt;</p>
             </div>
+            
           </div>
+
           <div class="usa-footer__contact-links mobile-lg:grid-col-6">
             <div class="usa-footer__social-links grid-row grid-gap-1">
+
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                ><img
-                    class="usa-social-link__icon"
-                    src="/assets/img/usa-icons/facebook.svg"
-                    alt="Facebook"
-                  /></a>
+                <a class="usa-social-link" href="javascript:void(0);">
+                <svg class="usa-social-link__icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/></svg> 
+                </a>
               </div>
+
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                ><img
-                    class="usa-social-link__icon"
-                    src="/assets/img/usa-icons/twitter.svg"
-                    alt="Twitter"
-                  /></a>
+                <a class="usa-social-link" href="javascript:void(0);">
+                <svg class="usa-social-link__icon" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19.912 7.925v.527A11.562 11.562 0 0 1 8.265 20.098 11.443 11.443 0 0 1 2 18.265c.328.017.657.017.985 0a8.132 8.132 0 0 0 5.093-1.698 4.092 4.092 0 0 1-3.786-2.852c.255.062.518.091.781.085.36-.002.72-.047 1.07-.136a4.109 4.109 0 0 1-3.21-4.024c.566.32 1.202.494 1.851.51a4.126 4.126 0 0 1-1.34-5.518 11.613 11.613 0 0 0 8.488 4.295 4.397 4.397 0 0 1-.119-.934 4.092 4.092 0 0 1 4.092-4.092 4.04 4.04 0 0 1 3.005 1.29 7.929 7.929 0 0 0 2.53-1c-.3.953-.94 1.763-1.8 2.274A7.725 7.725 0 0 0 22 5.803a8.489 8.489 0 0 1-2.088 2.122Z" fill-rule="evenodd"/></svg>
+                </a>
               </div>
+
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                ><img
-                    class="usa-social-link__icon"
-                    src="/assets/img/usa-icons/youtube.svg"
-                    alt="YouTube"
-                  /></a>
+                <a class="usa-social-link" href="javascript:void(0);">
+                <svg class="usa-social-link__icon" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19.816 5.389a2.469 2.469 0 0 1 1.766 1.746c.291 1.598.43 3.22.417 4.843a25.867 25.867 0 0 1-.417 4.794 2.468 2.468 0 0 1-1.766 1.795c-2.593.318-5.204.46-7.816.429a58.411 58.411 0 0 1-7.816-.429 2.469 2.469 0 0 1-1.766-1.746 25.879 25.879 0 0 1-.417-4.843 25.85 25.85 0 0 1 .417-4.793 2.468 2.468 0 0 1 1.766-1.796c2.594-.3 5.205-.427 7.816-.379a58.413 58.413 0 0 1 7.816.379ZM9.95 9.046v5.864l5.233-2.932L9.95 9.046Z"/></svg>
+                </a>
               </div>
+
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                ><img
-                    class="usa-social-link__icon"
-                    src="/assets/img/usa-icons/instagram.svg"
-                    alt="Instagram"
-                  /></a>
+                <a class="usa-social-link" href="javascript:void(0);">
+                <svg class="usa-social-link__icon" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8.273 3.063c-.958.045-1.612.198-2.184.423a4.41 4.41 0 0 0-1.592 1.04 4.409 4.409 0 0 0-1.035 1.596c-.222.572-.372 1.227-.414 2.185-.042.958-.052 1.266-.047 3.71.005 2.444.016 2.75.062 3.71.046.958.198 1.612.423 2.184a4.41 4.41 0 0 0 1.04 1.592 4.4 4.4 0 0 0 1.597 1.035c.572.221 1.227.372 2.185.414.958.042 1.266.052 3.71.047 2.443-.005 2.75-.016 3.71-.061.96-.046 1.611-.199 2.183-.423a4.411 4.411 0 0 0 1.592-1.04 4.41 4.41 0 0 0 1.035-1.597c.222-.572.372-1.227.414-2.184.042-.96.052-1.268.047-3.711-.005-2.444-.016-2.75-.061-3.71-.046-.96-.198-1.612-.423-2.184a4.418 4.418 0 0 0-1.04-1.592 4.4 4.4 0 0 0-1.597-1.035c-.572-.221-1.227-.372-2.185-.414-.958-.041-1.266-.052-3.71-.047-2.444.005-2.75.015-3.71.062m.105 16.27c-.878-.038-1.354-.184-1.672-.306a2.802 2.802 0 0 1-1.036-.671 2.772 2.772 0 0 1-.675-1.034c-.124-.317-.272-.793-.313-1.67-.045-.95-.054-1.234-.06-3.637-.004-2.403.005-2.687.046-3.636.038-.877.184-1.354.306-1.671.162-.421.357-.72.672-1.036a2.778 2.778 0 0 1 1.033-.675c.317-.124.793-.272 1.67-.313.95-.045 1.234-.054 3.637-.06 2.402-.005 2.687.004 3.637.046.876.038 1.354.183 1.67.306.421.162.72.357 1.037.671.316.315.511.613.675 1.034.124.317.271.792.313 1.67.045.95.055 1.234.06 3.636.004 2.402-.004 2.688-.046 3.636-.038.878-.184 1.354-.306 1.672a2.79 2.79 0 0 1-.672 1.036 2.78 2.78 0 0 1-1.033.675c-.317.124-.794.271-1.67.313-.95.045-1.234.054-3.637.06-2.403.005-2.687-.005-3.636-.046M15.715 7.19a1.08 1.08 0 1 0 2.16-.004 1.08 1.08 0 0 0-2.16.004m-8.336 4.819a4.62 4.62 0 1 0 9.242-.018 4.62 4.62 0 0 0-9.242.018M9 12.006a3 3 0 1 1 6-.012 3 3 0 0 1-6 .012"/></svg>
+                </a>
               </div>
+
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                ><img
-                    class="usa-social-link__icon"
-                    src="/assets/img/usa-icons/rss_feed.svg"
-                    alt="RSS"
-                  /></a>
+                <a class="usa-social-link" href="javascript:void(0);">
+                <svg class="usa-social-link__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><circle cx="6.18" cy="17.82" r="2.18"/><path d="M4 4.44v2.83c7.03 0 12.73 5.7 12.73 12.73h2.83c0-8.59-6.97-15.56-15.56-15.56zm0 5.66v2.83c3.9 0 7.07 3.17 7.07 7.07h2.83c0-5.47-4.43-9.9-9.9-9.9z"/></svg>
+                </a>
               </div>
+
             </div>
+
             <p class="usa-footer__contact-heading">
               &lt;Agency Contact Center&gt;
             </p>
+
             <address class="usa-footer__address">
               <div class="usa-footer__contact-info grid-row grid-gap">
+              
                 <div class="grid-col-auto">
                   <a href="tel:1-800-555-5555">&lt;(800) 555-GOVT&gt;</a>
                 </div>
@@ -131,6 +126,8 @@ export default function Footer(){
                 </div>
               </div>
             </address>
+
+
           </div>
         </div>
       </div>
