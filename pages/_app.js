167e555c520abc0715a5a58a5a80c3426142c5ee
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-10-06 01:09:28 +0530

    add auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../assets/css/styles.css'
 import '../styles/global.css'
 import '../styles/nav.scss'
+import { SessionProvider } from 'next-auth/react'
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return( <SessionProvider session={pageProps.session}>
+                <Component {...pageProps} />
+            </SessionProvider>
+         )
 }
