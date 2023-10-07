9b3bcf1234d12a4c4aadc311414dcd4d276fa36e
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-10-07 21:39:57 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
     if (!global._mongoClientPromise) {
         client = new MongoClient(uri, options);
-        global._mongoClientPromise = client.connect();
+    global._mongoClientPromise = client.connect();
     }
     clientPromise = global._mongoClientPromise
 } else {
