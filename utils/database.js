e7cd0ed2a9ac6aaff7094bf80ecf855a3e24115d
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-10-07 21:54:56 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
-const options = { useUnifiedTopology: true };
+const options = {};
 
 let client;
 let clientPromise;
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
     if (!global._mongoClientPromise) {
         client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+        global._mongoClientPromise = client.connect();
     }
     clientPromise = global._mongoClientPromise
 } else {
